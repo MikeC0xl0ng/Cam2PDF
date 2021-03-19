57e4da0a8a744305c9aa71e5f3d1f298ed398f58
MikeC0xl0ng <bodliev.mykola@gmail.com>
2021-03-19 22:06:05 +0100

    Making the camera working in the proper way (in the previous versions the app couldn't display frames in real time)

--- app/src/main/java/com/cam2pdf/MainActivity.java
+++ app/src/main/java/com/cam2pdf/MainActivity.java
@@ -1,48 +1,136 @@
 package com.cam2pdf;
 
-
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import android.graphics.Bitmap;
+import androidx.camera.core.CameraX;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureConfig;
+import androidx.camera.core.Preview;
+import androidx.camera.core.PreviewConfig;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.content.pm.PackageManager;
+import android.graphics.Matrix;
 import android.os.Bundle;
+import android.util.Rational;
+import android.util.Size;
+import android.view.Surface;
 import android.view.TextureView;
-import android.widget.ImageView;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
-    private Camera2Helper camera2Helper;
 
-    public Bitmap onImageAcquired(Bitmap imageIn) {
-        Bitmap bitmap = Bitmap.createBitmap(imageIn.getWidth(), imageIn.getHeight(), Bitmap.Config.ARGB_8888); // each pixel is stored on 4 bytes (ARGB_8888)
-        int w = imageIn.getWidth(), h = imageIn.getHeight();
-        for(int x=0; x<w; x++)
-            for(int y=0; y<h; y++)
-                bitmap.setPixel(x, y, imageIn.getPixel(x, h - y - 1));   // imageIn.getPixel(x, y) = getColor of that pixelint
-        return bitmap;
-    }
+    private final int REQUEST_CODE_PERMISSIONS = 0;
+    private final String[] REQUIRED_PERMISSIONS = new String[]{"android.permission.CAMERA", "android.permission.WRITE_EXTERNAL_STORAGE"};
+
+    private TextureView textureView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextureView textureView = findViewById(R.id.texture);
-        ImageView imageView = findViewById(R.id.imageView);
+        getSupportActionBar().hide();
+        textureView = (TextureView) findViewById(R.id.preview);
 
-        camera2Helper = new Camera2Helper(this, textureView, imageView);
+        if (permissionsGranted()){
+            startCamera();
+        }else{
+            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS);
+        }
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        if(camera2Helper != null)
-            camera2Helper.onResume();
+    private void startCamera(){
+        CameraX.unbindAll();
+
+        Rational aspect_ratio = new Rational(textureView.getWidth(), textureView.getHeight());
+        Size screen_size = new Size(textureView.getWidth(), textureView.getHeight());
+
+        PreviewConfig preview_config = new PreviewConfig.Builder()
+                .setTargetAspectRatio(aspect_ratio)
+                .setTargetResolution(screen_size)
+                .build();
+        Preview preview = new Preview(preview_config);
+        preview.setOnPreviewOutputUpdateListener(new Preview.OnPreviewOutputUpdateListener() {
+            @Override
+            public void onUpdated(Preview.PreviewOutput output) {
+                ViewGroup parent = (ViewGroup) textureView.getParent();
+                parent.removeView(textureView);
+                parent.addView(textureView);
+                textureView.setSurfaceTexture(output.getSurfaceTexture());
+
+                updateTransform();
+            }
+        });
+
+        ImageCaptureConfig image_capture_config = new ImageCaptureConfig.Builder()
+                .setCaptureMode(ImageCapture.CaptureMode.MIN_LATENCY)
+                .setTargetRotation(getWindowManager().getDefaultDisplay().getRotation())
+                .build();
+        ImageCapture img_capture = new ImageCapture(image_capture_config);
+        findViewById(R.id.takePhoto).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                File f = new File("sdcard/Camera_X" + System.currentTimeMillis());
+                img_capture.takePicture(f, new ImageCapture.OnImageSavedListener() {
+                    @Override
+                    public void onImageSaved(@NonNull File file) {
+
+                    }
+
+                    @Override
+                    public void onError(@NonNull ImageCapture.UseCaseError useCaseError, @NonNull String message, @Nullable Throwable cause) {
+
+                    }
+                });
+            }
+        });
+        CameraX.bindToLifecycle(this, preview, img_capture);
     }
 
-    @Override
-    public void onPause() {
-        if(camera2Helper != null)
-            camera2Helper.onPause();
-        super.onPause();
+    private void updateTransform(){
+        Matrix m = new Matrix();
+
+        float w = textureView.getMeasuredWidth();
+        float h = textureView.getMeasuredHeight();
+
+        float c_x = w/2f;
+        float c_y = h/2f;
+
+        int rotation_dgr;
+        int rotation = (int)textureView.getRotation();
+        switch(rotation){
+            case Surface.ROTATION_0:
+                rotation_dgr = 0;
+                break;
+            case Surface.ROTATION_90:
+                rotation_dgr = 90;
+                break;
+            case Surface.ROTATION_180:
+                rotation_dgr = 180;
+                break;
+            case Surface.ROTATION_270:
+                rotation_dgr = 270;
+                break;
+            default:
+                return;
+        }
+        m.postRotate((float)rotation_dgr, c_x, c_y);
+        textureView.setTransform(m);
     }
+
+    private boolean permissionsGranted(){
+        for (String permission : REQUIRED_PERMISSIONS){
+            if (ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_DENIED){
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
-
-
